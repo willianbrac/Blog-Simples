596fdb13b97c949e23389addff19b4d41a4e48c9
willianbrac <66275588+willianbrac@users.noreply.github.com>
2021-04-05 22:37:54 -0500

    Resolvendo problemas

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const articlesController   = require("./articles/ArticlesController");
 const categoryModel        = require("./categories/CategoryModel");
 const articleModel         = require("./articles/ArticleModel");
 const Article = require("./articles/ArticleModel");
-const Category = require("./categories/CategoryModel");
-
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
@@ -32,10 +30,10 @@ app.use("/", articlesController  );
 
 
 app.get("/", (req, res)=>{
-    Article.findAll({
+    articleModel.findAll({
         order: [['id','DESC']]
     }).then(articles => {
-        Category.findAll().then(categories => {
+        categoryModel.findAll().then(categories => {
             res.render('index', {articles: articles, categories: categories});
         })
     })
@@ -43,21 +41,39 @@ app.get("/", (req, res)=>{
 
 app.get("/:slug", (req,res) => {
     var slug = req.params.slug;
-    Article.findOne({
+    articleModel.findOne({
         where: {
             slug: slug
         }
     }).then(article => {
         if (article != undefined) {
-            Category.findAll().then(categories => {
+            categoryModel.findAll().then(categories => {
                 res.render('article', {article: article, categories: categories});
             })          
         } else {
             res.redirect("/");
         }
     }).catch( err => {
-        console.log("MESAGE ERROR => " + err);
+        console.log("MESSAGE ERROR => " + err);
     })
-})
+});
+
+app.get("/category/:slug", (req,res) => {
+    var slug = req.params.slug;
+    categoryModel.findOne({
+        where: {slug: slug },
+        include: [{model: articleModel}]
+    }).then(category => {
+        if (category != undefined) {
+            categoryModel.findAll().then(categories => {
+                res.render('index', {articles: category.articles, categories: categories});
+            }) 
+        } else {
+            res.redirect("/");
+        }
+    }).catch( err => {
+        console.log("MESSAGE ERROR => " + err);
+    })
+});
 
 module.exports = app;
